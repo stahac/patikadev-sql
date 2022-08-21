f760d41b11197cbac6a126c1ebdd372214cfa26f
stahac <111694466+stahac@users.noreply.github.com>
2022-08-21 17:17:37 +0200

    Create odev1-4.sql

---
+++ odev1-4.sql
@@ -0,0 +1,4 @@
+(What is the value in the last_name column of the customer whose value is 'Mary' in the first_name column in the customer table?)
+
+SELECT last_name FROM customer
+WHERE first_name = ''Mary';
